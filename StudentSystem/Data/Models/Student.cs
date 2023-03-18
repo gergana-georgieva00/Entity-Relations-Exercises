64434e901d2b82c4b0f09578318bc9d4316e6c55
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-18 13:55:24 +0200

    Ex 1

---
+++ StudentSystem/Data/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? BirthDay { get; set; }
+    }
+}
