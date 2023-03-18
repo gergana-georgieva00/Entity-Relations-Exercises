734c78e7062ea94189d7ff76333dbcb19ed21fb0
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-18 14:53:36 +0200

    Ex 1

--- StudentSystem/Data/StudentSystemContext.cs
+++ StudentSystem/Data/StudentSystemContext.cs
@@ -3,6 +3,7 @@ using P01_StudentSystem.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,9 +28,29 @@ namespace P01_StudentSystem.Data
             modelBuilder.Entity<Student>(e =>
             {
                 e.HasKey(s => s.StudentId);
-                e.Property(s => s.Name).HasMaxLength(100);
+                e.Property(s => s.Name).HasMaxLength(100).IsUnicode();
                 e.Property(s => s.PhoneNumber).HasMaxLength(10).IsUnicode(false).IsRequired(false);
                 e.Property(s => s.BirthDay).IsRequired(false);
+            });
+
+            modelBuilder.Entity<Course>(e =>
+            {
+                e.HasKey(c => c.CourseId);
+                e.Property(c => c.Name).HasMaxLength(80).IsUnicode();
+                e.Property(c => c.Description).IsUnicode().IsRequired(false);
+            });
+
+            modelBuilder.Entity<Resource>(e =>
+            {
+                e.HasKey(r => r.ResourceId);
+                e.Property(r => r.Name).HasMaxLength(50).IsUnicode();
+                e.Property(r => r.Url).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Homework>(e =>
+            {
+                e.HasKey(h => h.HomeworkId);
+                e.Property(h => h.Content).IsUnicode(false);
             });
         }
     }
