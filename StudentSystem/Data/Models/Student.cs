44f99b847c2547ba73480128d84c602c06f40133
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-18 14:07:49 +0200

    Ex 1

--- StudentSystem/Data/Models/Student.cs
+++ StudentSystem/Data/Models/Student.cs
@@ -11,12 +11,18 @@ namespace P01_StudentSystem.Data.Models
         // One student can have many Courses 
         // One student can have many Homeworks 
 
+        public Student()
+        {
+            StudentCourses = new HashSet<StudentCourse>();
+            Homeworks = new HashSet<Homework>();
+        }
+
         public int StudentId { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime RegisteredOn { get; set; }
         public DateTime? BirthDay { get; set; }
-        public ICollection<Course> Courses { get; set; }
+        public ICollection<StudentCourse> StudentCourses { get; set; }
         public ICollection<Homework> Homeworks { get; set; }
     }
 }
